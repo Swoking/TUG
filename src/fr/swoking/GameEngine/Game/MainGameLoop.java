9a662862dc39120305952ec123f964531086f015
Swoking <rgab77370@live.fr>
2016-11-21 17:59:01 +0100

    render + test

--- src/fr/swoking/GameEngine/Game/MainGameLoop.java
+++ src/fr/swoking/GameEngine/Game/MainGameLoop.java
@@ -3,6 +3,9 @@ package fr.swoking.GameEngine.Game;
 import org.lwjgl.opengl.Display;
 
 import fr.swoking.GameEngine.RenderEngine.DisplayManager;
+import fr.swoking.GameEngine.RenderEngine.Loader;
+import fr.swoking.GameEngine.RenderEngine.RawModel;
+import fr.swoking.GameEngine.RenderEngine.Renderer;
 
 public class MainGameLoop {
 
@@ -10,14 +13,32 @@ public class MainGameLoop {
 		
 		DisplayManager.createDisplay();
 		
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+		
+		float[] vertices = {
+				// Left bottom triangle
+				-0.5f, 0.5f, 0f,
+				-0.5f, -0.5f, 0f,
+				0.5f, -0.5f, 0f,
+				// Right bottom triangle
+				0.5f, -0.5f, 0f,
+				0.5f, 0.5f, 0f,
+				-0.5f, 0.5f, 0f
+		};
+		
+		RawModel model = loader.loadToVAO(vertices);
+		
 		while(!Display.isCloseRequested()){
 			
+			renderer.prepare();
 			//game logic
-			//render
+			renderer.render(model);
 			DisplayManager.updateDisplay();
 			
 		}
 		
+		loader.cleanUP();
 		DisplayManager.closeDisplay();
 	}
 	
